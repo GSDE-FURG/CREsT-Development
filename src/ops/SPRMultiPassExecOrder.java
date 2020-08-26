5f6f1a4dbe94ce5537a3c2295cc8c4881f5c3b49
claytonfariasEC <47605415+claytonfariasEC@users.noreply.github.com>
2020-08-26 16:42:32 -0300

    developing

--- src/ops/SPRMultiPassExecOrder.java
+++ src/ops/SPRMultiPassExecOrder.java
@@ -1,33 +1,109 @@
 package ops;
 
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+import java.math.BigDecimal;
+
 import javax.script.ScriptException;
 
-class Teste{
+import datastructures.CellLibrary;
+//import jdk.internal.jline.Terminal;
+import signalProbability.ProbCircuit;
+import tool.Commands;
 
-    public static void main(final String[] args) {
+/**
+ *
+ * @author Clayton
+ */
+class main_SPRMP_Exec {
+
+    public static void main(final String[] args) throws IOException, ScriptException {
         System.out.println("\n\nHello world, working");
+        String circuitPath = "c17_cadence.v";
+        String reliability = "0.999";
+        String mode = "SPRMP";
 
-        SPRMultiPassExecOrder objeto = new  SPRMultiPassExecOrder (10);
-        
-        objeto.teste();
-        
-   }
-       
+        final SPRMultiPassExecOrder objeto = new SPRMultiPassExecOrder(reliability, mode, circuitPath, "cadence.genlib");
+
+        objeto.getReliabilitySPR_MP();
+
+    }
 
 }
 
-public class SPRMultiPassExecOrder{
+/**
+ *
+ * @author Clayton
+ */
+public class SPRMultiPassExecOrder {
+
+    private String reliability;
+    private String mode;
+    private String circuitFilePath;
+    private String genlib;
 
 
-    private final int key;
+    public SPRMultiPassExecOrder(String reliabilty, String mode, String circuitFilePath, String genlib) {
 
-    public SPRMultiPassExecOrder(int entry) {
-            this.key = entry;
+        this.reliability = reliabilty;
+        this.circuitFilePath = circuitFilePath;
+        this.mode = mode;
+        this.genlib = genlib;
+
     }
-   
-    public void teste(){
-        System.out.println("beyyy " + this.key);
+
+    public void getReliabilitySPR_MP()
+            throws Exception {
+
+        System.out.println("SPRMP Development : " + this.circuitFilePath + "   -   "  + this.reliability);
+        
+        final long startTime = System.currentTimeMillis();
+        
+        final Commands cmd = new Commands();
+
+        //Reading library and verilog file (extension .v)
+        cmd.ReadGenlib(this.genlib);
+
+        cmd.ReadVerilog(this.circuitFilePath);
+
     }
-
-
 }
+
+       /*
+        
+        Terminal Terminal = Terminal.getInstance(); 
+
+
+        String result = "";
+        CellLibrary cellLib = Terminal.getInstance().getCellLibrary();
+        ProbCircuit pCircuit = Terminal.getInstance().getProbCircuit();         
+        pCircuit.clearProbSignalsMatrix();
+        cellLib.setPTMCells2(Float.valueOf(reliability));
+        cellLib.setPTMCells(new BigDecimal(reliability));
+        pCircuit.setPTMReliabilityMatrix();
+        pCircuit.setProbSignalStates(false);
+        
+        switch(type) {
+            
+            case "big_decimal":        
+                result = "Reliability SPR_MP (in BigDecimal) of " + pCircuit.getName() + " CIRCUIT is " + SPRMultiPassV3Ops.getSPRMultiPassReliaiblity(pCircuit);
+                break;
+            
+            case "float":                                          
+                result = "Reliability SPR (in float) of "; //+ pCircuit.getName() + " CIRCUIT is " + SPROpsFloat.getSPRReliability(pCircuit);
+                break;
+                        
+        }               
+        
+        //Terminal.getInstance().terminalOutput(result);
+        System.out.println(result);
+        
+        final long endTime = System.currentTimeMillis();
+        String timeConsup = "## TIME CONSUPTION ## ==> " + Long.toString((endTime - startTime)) + " ms";
+        //Terminal.getInstance().terminalOutput(timeConsup);               
+    }
+
+    */
+    
+
+
